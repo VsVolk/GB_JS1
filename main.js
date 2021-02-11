ab74a226772a52bac3ea2bc186abeb792a3ca917
Stas <volkov191191@gmail.com>
2021-02-11 19:43:59 +0300

    homeWork4

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-
-//Вывести простые числа
-var i=1;
-while (i<=100) {
-i++;
-for(var j=2; j<=i; j++){
-    if (i%j == 0)
-    break;
-}
-if(j==i)
-console.log(i);
-}
-
-//стоимость корзины
+/* //стоимость корзины
 let arr = [
     24, 49.8, 
     60.6, 37.3,
@@ -27,12 +14,51 @@ function countBasketPrice (array) {
 
 countBasketPrice(arr);
 
-// вывод 0-9 не используя тело цикла
-for (var i = 0; i<10; console.log(i++)) {
 
+//функция, преобразующую число в объект.
+let number = prompt ('Введите трехзначное число: ')
+function CreateObject (number) {
+    if (number<=999) {
+    this.s =  Math.floor(number/100%10);
+    this.d = Math.floor(number/10%10);
+    this.e = number%10;
+    } else 
+    console.log("Вы ввели не трехзначное число");
+} */
+
+/* console.log(new CreateObject(number)); */
+
+/* let hockeyProduct = {
+    vendorCode:,
+    price: ,
+    quantity: ,
+    payMethod: ,
+    delivery: ,
+    productType = {
+        skates: ,
+        helmets: ,
+        defense: ,
+        hockeySticks: ,
+        thermalUnderwear: ,
+    }
+} */
+
+let stics = {
+    name: "ccm",
+    height: 150,
+    price: 15600,
+    flex: 76,
+    type: "left",
 }
 
-//Ёлочка
-for (let x = "x"; x.length<=20; x+="x") {
-    console.log(x);
+let shields = {
+    name: "bauer",
+    size: 36,
+    price: 10400,
 }
+
+function countBasketPrice () {
+    let sum = stics.price + shields.price
+    console.log(sum); 
+}
+countBasketPrice();
